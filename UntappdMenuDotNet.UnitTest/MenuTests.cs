73a1ffb0f9b7e5e9557ded90abc68f6e5d21e192
khnbb <kherrmann@newbelgium.com>
2021-03-23 11:01:08 -0600

    Tests added

---
+++ UntappdMenuDotNet.UnitTest/MenuTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UntappdMenuDotNet.UnitTest
+{
+    [TestClass]
+    public class MenuTests
+    {
+        private string email = "validemail@domain.com";
+        private string token = "validtoken";
+
+        [ExpectedException(typeof(ArgumentNullException), "An ArgumentNullException was expected but one was not thrown")]
+        [TestMethod]
+        public void NoAuthBytes()
+        {
+            _ = new UntappdService(null);
+        }
+
+        [ExpectedException(typeof(ArgumentNullException), "An ArgumentNullException was expected but one was not thrown")]
+        [TestMethod]
+        public void NoAuthStrings()
+        {
+            _ = new UntappdService(null, null);
+        }
+
+        [ExpectedException(typeof(UntappdException), "An UntappdException was expected but one was not thrown")]
+        [TestMethod]
+        public async Task MalformedAuthBytes()
+        {
+            var service = new UntappdService(new byte[] { 1, 2, 3 });
+            _ = await service.GetLocationCollection();
+        }
+
+        [ExpectedException(typeof(UntappdException),"An UntappdException was expected but one was not thrown")]
+        [TestMethod]
+        public async Task MalformedAuthStrings()
+        {
+            var service = new UntappdService("fake@nothing.net", "incorrect");
+            _ = await service.GetLocationCollection();
+        }
+
+        [TestMethod]
+        public async Task Locations()
+        {
+            var service = new UntappdService(email, token);
+            var locationCollection = await service.GetLocationCollection();
+            Assert.IsNotNull(locationCollection);
+            Assert.IsNotNull(locationCollection.Locations);
+        }
+    }
+}
---
+++ UntappdMenuDotNet.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UntappdMenuDotNet.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UntappdMenuDotNet.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ad84e1ba-f5e3-45c1-835a-e0836585253b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
